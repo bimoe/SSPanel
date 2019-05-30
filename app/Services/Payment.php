73319c10f7faf0d557ddf377185805009db23f30
Bimoe <9499460@gmail.com>
2019-05-31 03:43:17 +0800

    no message

---
+++ app/Services/Payment.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tonyzou
+ * Date: 2018/9/24
+ * Time: 下午7:07
+ */
+
+namespace App\Services;
+
+use App\Services\Config;
+use App\Services\Gateway\{
+    AopF2F, Codepay, DoiAMPay, PaymentWall, ChenPay, SPay, TrimePay, TomatoPay, FlyfoxPay, BitPay
+};
+
+class Payment
+{
+    public static function getClient(){
+        $method = Config::get("payment_system");
+        switch($method){
+            case("codepay"):
+                return new Codepay();
+            case("doiampay"):
+                return new DoiAMPay();
+            case("paymentwall"):
+                return new PaymentWall();
+            case("spay"):
+                return new SPay();
+            case("f2fpay"):
+                return new AopF2F();
+            case("chenAlipay"):
+                return new ChenPay();
+            case("trimepay"):
+                return new TrimePay(Config::get('trimepay_secret'));
+	        case("tomatopay"):
+                return new TomatoPay();
+	        case("flyfoxpay"):
+                return new FlyfoxPay();
+            case("bitpay"):
+                return new BitPay(Config::get('bitpay_secret'));
+            default:
+                return NULL;
+        }
+    }
+
+    public static function notify($request, $response, $args){
+        return self::getClient()->notify($request, $response, $args);
+    }
+
+    public static function returnHTML($request, $response, $args){
+        return self::getClient()->getReturnHTML($request, $response, $args);
+    }
+
+	public static function purchaseHTML(){
+		if (self::getClient() != NULL) {
+			return self::getClient()->getPurchaseHTML();
+		} else {
+			return '';
+		}
+    }
+
+    public static function getStatus($request, $response, $args){
+        return self::getClient()->getStatus($request, $response, $args);
+    }
+
+    public static function purchase($request, $response, $args){
+        return self::getClient()->purchase($request, $response, $args);
+    }
+}
